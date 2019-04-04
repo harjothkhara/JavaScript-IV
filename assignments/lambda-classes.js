a0a4d7a73d8e20c3e33d58cddf183594504a8c70
harjothkhara <harjoth.khara@gmail.com>
2019-04-04 16:54:55 -0700

    instructor class added

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -13,13 +13,49 @@ class Person {
     
   }// Person (baseclass)
 
-  const clark = new Person({
-    age: 28,
-    name: "Clark",
-    location: "Smallville",
-    gender: "Male"
+  //test - person class
+//   const clark = new Person({
+//     age: 28,
+//     name: "Clark",
+//     location: "Smallville",
+//     gender: "Male"
+//   });
+
+//   console.log(clark);
+//   console.log(clark.speak());
+
+  class Instructor extends Person{
+    constructor(InstructorAtt){
+     super (InstructorAtt);
+     this.newSpeciality = InstructorAtt.speciality; //unique
+     this.newfavLanguage = InstructorAtt.favLanguage; //unique
+     this.newcatchPhrase = InstructorAtt.catchPhrase; //unique 
+    }
+    // Methods
+    demo (subject) {
+      console.log(`Today we are learning about ${subject}`);
+    }
+    
+    grade (student,subject) {
+     console.log(`${student.name} receives a perfect score on ${subject}`);
+    }
+  }// Instructor
+
+  const josh = new Instructor({
+    age: 35,
+    name: "Josh",
+    location: "Utah",
+    gender: "Male",
+    speciality: "front-end",
+    favLanguage:"CSS",
+    catchPhrase: "you got this!"
   });
   
-  // Person
-console.log(clark);
-console.log(clark.speak());
+  console.log(josh); 
+  console.log(josh.demo("CSS")); //unique method
+  console.log(josh.speak()); //inherited from person "parent"
+
+
+
+
+  
