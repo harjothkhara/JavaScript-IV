d7f39c4e98f64224760314a5807870c7e420929f
harjothkhara <harjoth.khara@gmail.com>
2019-04-04 14:59:08 -0700

    lambda classes: person

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,25 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attributes){
+      this.newName = attributes.name;
+      this.newAge = attributes.age;
+      this.newLocation = attributes.location;
+      this.newGender = attributes.gender;
+    }
+    // Methods
+    speak() {
+      return `Hello I'm ${this.newName}, and I'm from ${this.newLocation}`;
+     }
+    
+  }// Person (baseclass)
+
+  const clark = new Person({
+    age: 28,
+    name: "Clark",
+    location: "Smallville",
+    gender: "Male"
+  });
+  
+  // Person
+console.log(clark);
+console.log(clark.speak());
